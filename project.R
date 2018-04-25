8a3138dc1eca6e15fea5413335709d2c6dac08cf
brian.weinfeld+cuny@gmail.com <brian.weinfeld+cuny@gmail.com>
2018-04-25 18:38:45 -0400

    collected data

--- project.R
+++ project.R
@@ -10,6 +10,8 @@ library(directlabels)
 #Is there a relationship between the number of shots taken by both teams, the period and the number of goals scored?
 #goals ~ shots + period
 
+
+# data collection ---------------------------------------------------------
 games.data <- GET('https://api.mysportsfeeds.com/v1.2/pull/nhl/2015-2016-regular/full_game_schedule.json', authenticate('njpsy', 'asdfasdf'), 
             add_headers('Content-Type'='application/json', 'Accept-Encoding'='gzip')) %>%
   content(as='text', encoding='UTF-8') %>%
@@ -48,47 +50,31 @@ box.data <- game.dates$date %>%
   mutate(id = as.character(id),
          period = as.factor(period)) %>%
   as.tibble()
-
-fit <- lm(goals ~ shots + period, data=box.data)
-
-summary(fit)
-
-coefficients(fit) # model coefficients
-confint(fit, level=0.95) # CIs for model parameters 
-fitted(fit) # predicted values
-residuals(fit) # residuals
-anova(fit) # anova table 
-vcov(fit) # covariance matrix for model parameters 
-influence(fit) # regression diagnostics
+# data collection ---------------------------------------------------------
 
 
-ggplot(spread.data) +
-  geom_histogram(aes(x=shots, fill=factor(reorder(goals, -goals))), bins=35) +
-  labs(x='Shots',
-       y='Frequency',
-       title='Distribution of Shots per Period',
-       fill='Goals Scored') +
-  scale_fill_brewer(palette='YlOrRd', direction=-1)
-
+# explore individual data -------------------------------------------------
 box.data %>%
-  filter(period == '1') %>%
-  select(goals) %>%
-  describe()
-
-box.data %>%
-  filter(period == '2') %>%
-  select(goals) %>%
-  describe()
-
-box.data %>%
-  filter(period == '3') %>%
-  select(goals) %>%
-  describe()
-
-summary.box.data <- box.data %>%
   group_by(period) %>%
-  summarize(m = mean(goals)) %>%
-  rename(period2 = period)
+  summarize(min = min(shots),
+            max = max(shots))
+
+box.data %>%
+  mutate(period = paste0('Period ', period)) %>%
+  ggplot() +
+  geom_histogram(aes(shots, fill=period), bins=37) +
+  facet_wrap(~period, ncol=1) +
+  labs(x=NULL,
+       y=NULL,
+       title='Frequency of Shots by Period') +
+  scale_x_continuous(limits=c(0, 40), breaks=seq(0, 40, 5)) +
+  theme_bw() + 
+  theme(legend.position='None',
+        strip.background=element_rect(fill='grey70'),
+        strip.text=element_text(color='black', size=12),
+        axis.text=element_text(size=10),
+        panel.grid.minor=element_blank(),
+        panel.grid.major.x=element_blank())
 
 box.data %>%
   mutate(period=paste('Period ', period)) %>%
@@ -106,31 +92,7 @@ box.data %>%
         strip.text=element_text(color='black', size=12),
         axis.text=element_text(size=10),
         panel.grid.minor=element_blank(),
-        panel.grid.major.x=element_blank())
-
-
-ggplot(box.data) +
-  geom_density(aes(x=goals, fill=period, color=period), alpha=0.2, adjust=3)  +
-  geom_vline(data=summary.box.data, aes(xintercept=m, color=period2)) +
-  scale_x_discrete(limits=0:7, breaks=0:7, expand=c(0, 0)) + 
-  scale_color_discrete(breaks=NULL) + 
-  scale_y_continuous(expand=c(0,0,.01,.01)) +
-  labs(x='Goals', 
-       y='Proportion of Goals Scored',
-       fill='Period',
-       title='Proportion of Goals Scored') +
-  guides(fill=guide_legend(ncol=3, label.position='top')) +
-  theme_bw() + 
-  theme(legend.position=c(.9, .8), 
-        legend.justification=c(1, 1), 
-        legend.background=element_rect(color='darkblue'),
-        legend.text=element_text(size=16),
-        legend.title=element_text(size=16),
-        legend.title.align=0.5,
-        legend.text.align=0.5,
-        legend.key.size=unit(0.8, 'cm'),
-        axis.text=element_text(size=12))
-
+        panel.grid.major.x=element_blank())  
 
 ggplot(box.data, aes(fill=factor(goals), group=goals)) +
   geom_bar(aes(x=period), position=position_fill(reverse=TRUE)) +
@@ -142,3 +104,62 @@ ggplot(box.data, aes(fill=factor(goals), group=goals)) +
        y=NULL,
        title='Makeup of Scoring by Period') + 
   guides(fill=guide_legend(reverse=TRUE))
+# explore individual data -------------------------------------------------
+
+
+# regression fit ----------------------------------------------------------
+
+fit <- lm(goals ~ shots + period, data=box.data)
+
+summary(fit)
+
+#goals = 0.066448*shots + 0.285678*period2 + 0.472701*period3 + 0.185791
+#shots range 7-40
+
+coefficients(fit) # model coefficients
+confint(fit, level=0.95) # CIs for model parameters 
+fitted(fit) # predicted values
+residuals(fit) # residuals
+anova(fit) # anova table 
+vcov(fit) # covariance matrix for model parameters 
+influence(fit) # regression diagnostics
+
+# regression fit ----------------------------------------------------------
+
+
+# precondition check ------------------------------------------------------
+
+ggplot() +
+  geom_point(aes(1:3690, fit$residuals)) +
+  geom_hline(yintercept=0, color='yellow')
+
+#3 more
+
+# precondition check ------------------------------------------------------
+
+
+
+
+box.data %>%
+  filter(period == '1') %>%
+  select(goals) %>%
+  describe()
+
+box.data %>%
+  filter(period == '2') %>%
+  select(goals) %>%
+  as.matrix() %>%
+  describe()
+
+box.data %>%
+  filter(period == '3') %>%
+  describe()
+
+
+
+
+
+
+
+
+
